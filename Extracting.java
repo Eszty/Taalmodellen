dd89274edf942910f17c13b1ed437f1ae19f7899
Eszter Fodor <chytos@gmail.com>
2012-04-08 21:25:06 +0200

    begin gemaakt aan java

--- Extracting.java
+++ Extracting.java
@@ -10,11 +10,22 @@ Eszter Fodor & Sharon Gieske
 	- return the m most frequence sequence
 */ 
 
+import java.util.*;
+
 public class Extracting 
 {
 	public static void main(String[] args)
 	{
-			
+		try
+		{
+			TextIO.readFile("ovis-trainset.txt"); //reads in the file 
+			System.out.printf("done");
+		}
+		catch (Exception e)
+		{
+			System.out.println("fail");
+		}
+							
 	}
 }
 
