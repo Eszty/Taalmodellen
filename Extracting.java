8f13cf74571465703500b41f436e99a257c7af13
Eszter Fodor <chytos@gmail.com>
2012-04-08 20:30:19 +0200

    java file aangemaakt, nog niet echt begonnen

---
+++ Extracting.java
@@ -0,0 +1,35 @@
+/* Program for extracting ngrams and counting word frequencies
+Eszter Fodor & Sharon Gieske
+*/
+
+/* TODO:
+	- INPUT: file (ovis-trainset.txt) + integers (n => ngrams, m => m most frequent sequences) 
+	- read file
+	- search for the ngrams
+	- count each sequence's frequency
+	- return the m most frequence sequence
+*/ 
+
+public class Extracting 
+{
+	public static void main(String[] args)
+	{
+			
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
