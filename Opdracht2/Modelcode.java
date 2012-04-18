c7be27a8a5079784b481e78bfb7af152346369b2
Eszter Fodor <chytos@gmail.com>
2012-04-18 21:04:49 +0200

    organisatie

---
+++ Opdracht2/Modelcode.java
@@ -0,0 +1,254 @@
+import java.util.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+
+public class Modelcode {
+ /*
+  * Input: 1. Name of the file to get the n-grams from 2. Size of the n-grams
+  * 3. Amount of highest counts to be printed
+  * 
+  * First get all sentences from the file. Then get and count the n-grams in
+  * those sentences. After that sort the n-grams to get the highest counts.
+  * ovis-trainset.txt 2 10
+  */
+public static void main(String[] args) throws IOException {
+
+	 String filename = args[0];
+	 int n = Integer.parseInt(args[1]);
+	 int m = Integer.parseInt(args[2]);
+
+	 File file = new File(filename);
+	 StringTokenizer sentences = getSentences(file);
+	 StringTokenizer sentences1 = getSentences(file);
+	 HashMap<String, Integer> nGramCounts = wordsCounter(sentences, n);
+
+	 int p = n-1;
+	 HashMap<String, Integer> n_1GramCounts = wordsCounter(sentences1, p);
+
+	 HashMap<String, Integer> sortedNgrams = new LinkedHashMap<String, Integer>();
+	 HashMap<String, Integer> sortedN_1grams = new LinkedHashMap<String, Integer>();
+
+	 printngrams(nGramCounts, sortedNgrams, m);	
+	 printngrams(n_1GramCounts, sortedN_1grams, m);	
+	calculateprob(sortedNgrams,sortedN_1grams);
+ }
+
+/*
+Calculates probability for testsentences
+*/
+public static void calculateprob( HashMap<String, Integer> sortedNgrams,  HashMap<String, Integer> sortedN_1grams) throws IOException{
+	 File file = new File("lorem.txt");
+	 StringTokenizer sentences = getSentences(file);
+	 HashMap<String, Integer> probshash = new LinkedHashMap<String, Integer>();
+
+	 System.out.println("NOW");
+	 String[] sentenceArray = new String[sentences.countTokens()];
+	 String[] subsentenceArray = new String[sentences.countTokens()];
+
+	// create string array from sentences in testfile
+	 int j = 0;
+ 	 String empty = "";
+	 while(sentences.hasMoreTokens()){
+		sentenceArray[j] = sentences.nextToken();	
+		sentenceArray[j] = empty.concat(" " + sentenceArray[j]);
+		j++;
+	 }
+
+	// create n-1 strings
+	for(int k = 0; k < sentenceArray.length; k++)
+	{
+		String[] words = sentenceArray[k].split(" "); 
+		subsentenceArray[k] = "";
+		for(int p = 0; p < (words.length-1); p++){
+			if(p ==0)
+				subsentenceArray[k] = subsentenceArray[k].concat(words[p]);
+			else
+				subsentenceArray[k] = subsentenceArray[k].concat(" " + words[p]);
+		}
+
+		System.out.println("NOOOOO");
+
+		System.out.println(sentenceArray[k]);
+
+		System.out.println(subsentenceArray[k]);
+
+	}
+	for(int i = 0; i < sentenceArray.length; i++){
+		System.out.println(sentenceArray[i]);
+	//	String a = sentenceArray[i];
+		//String b = subsentenceArray[i];
+		Integer val = 0;
+		Integer subval = 0;
+			//iterate through hashmap
+			for(Map.Entry<String, Integer> entry : sortedNgrams.entrySet())
+			{
+
+				String key = (String) entry.getKey();
+				//System.out.println(key);
+				if(key.equals(sentenceArray[i])){
+					val = entry.getValue();
+				}
+			}
+
+			for(Map.Entry<String, Integer> entry : sortedN_1grams.entrySet())
+			{
+
+				String key = (String) entry.getKey();
+				//System.out.println(key);
+				if(key.equals(subsentenceArray[i])){
+					subval = entry.getValue();
+				}
+			}
+		System.out.printf("%d : %d / %d",i,val, subval);
+
+	}
+
+
+}
+
+public static void printngrams(HashMap<String, Integer> nGramCounts, HashMap<String, Integer> sortedNgrams, int m){
+
+	 /*
+	  * Sorting the created n-gram hashmap with counters
+	  */
+	 List<String> nGrams = new ArrayList<String>(nGramCounts.keySet());
+
+	 List<Integer> yourMapValues = new ArrayList<Integer>(
+	 nGramCounts.values());
+
+
+	 TreeSet<Integer> sortedSet = new TreeSet<Integer>(yourMapValues);
+	 Object[] sortedArray = sortedSet.toArray();
+	 int size = sortedArray.length;
+
+	 for (int i = size - 1; i >= 0; i--) {
+	 	sortedNgrams.put(nGrams.get(yourMapValues.indexOf(sortedArray[i])),
+	 	(Integer) sortedArray[i]);
+	 }
+
+	 /*
+	  * Printing the top m n-grams or all of them if m is bigger than the
+	  * n-gram set
+	  */
+	 Iterator<String> it = sortedNgrams.keySet().iterator();
+	 int i = 0;
+
+
+	 while (it.hasNext()) {
+	 	String nGram = (String) it.next();
+	 	Integer count = sortedNgrams.get(nGram);
+
+	 	if (i < m) {
+	 		i++;
+	 		System.out.println(i + ". " + nGram + ": " + count);
+	 	}
+	 }
+	System.out.println("HELLO");
+}
+
+ /*
+  * Read the input file, and split every sentence on a newline using a
+  * deliminator "\n" Returns a StringTokenizer containing the sentences
+  */
+public static StringTokenizer getSentences(File file) throws IOException {
+ 
+	StringBuffer contents = new StringBuffer();
+	BufferedReader reader = null;
+
+ 	try {
+ 		reader = new BufferedReader(new FileReader(file));
+ 		String text = null;
+
+ 		while ((text = reader.readLine()) != null) {
+ 			contents.append(text).append(
+ 			System.getProperty("line.separator"));
+ 		}
+
+ 	} 
+	catch (FileNotFoundException e) {
+ 		e.printStackTrace();
+ 	}
+
+ 	String message = contents.substring(0, contents.length());
+ 	String deliminator = "\t\n\r\f";
+ 	StringTokenizer st = new StringTokenizer(message, deliminator);
+
+ 	return st;
+}
+ /*
+  * Method used to create a sorted hashMap with n-grams of size n from the
+  * inputed StringTokenizer st Creates an String array of size n to
+  * temporarily hold individual words so we can still use st.nextToken and
+  * not lose data Gets every sentence from the st, converts it into a
+  * correctedsentence StringTokenizer st2. Fill temp with the first n words
+  * of st2. Create ngram using concate. Put ngram in hashMap or add 1 to the
+  * value of n-gram in the hashMap. Grab the next token, delete the oldest
+  * token in temp, move the rest i to the left, put grabbed token as newest
+  * token. Create ngram over temp values. Check if in hashMap , if not add
+  * else increase value by 1 Repeat untill no more tokens in st2 or st.
+  */
+public static HashMap<String, Integer> wordsCounter(StringTokenizer st, int n) {
+ 	HashMap<String, Integer> ngramMap = new HashMap<String, Integer>();
+ 	String sentence;
+ 	String[] temp = new String[n];
+
+
+ 	while (st.hasMoreTokens()) {
+
+ 		String ngram = "";
+ 		sentence = st.nextToken();
+		String[] words = sentence.split(" "); 
+		System.out.println(words.length);
+ 		sentence = "<s> " + sentence;
+ 		for (int i = 0; words.length+i <n-1  ; i++) {
+ 			sentence = "<s> " + sentence;	
+ 		}
+
+ 		sentence = sentence + " </s>";
+		System.out.println(sentence);
+ 		StringTokenizer st2 = new StringTokenizer(sentence, " ");
+
+ 		for (int c = 0; c < n; c++) {
+ 			temp[c] = st2.nextToken();
+ 			// fix
+ 			}
+
+ 		for (int i = 0; i < n; i++) {
+ 			ngram = ngram.concat(" " + temp[i]);
+ 		}
+		System.out.println("BALABALLA");
+ 		if (ngramMap.containsKey(ngram)) {
+ 			ngramMap.put(ngram, (ngramMap.get(ngram) + 1));
+ 		} 
+		else {
+ 			ngramMap.put(ngram, 1);
+ 		}
+
+ 			while (st2.hasMoreTokens()) {
+ 				ngram = "";
+
+ 				for (int c2 = 0; c2 < n - 1; c2++) {
+ 					temp[c2] = temp[c2 + 1];
+ 				}
+
+ 				temp[n - 1] = st2.nextToken();
+
+ 				for (int i = 0; i < n; i++) {
+ 					ngram = ngram.concat(" " + temp[i]);
+ 				}
+
+ 				if (ngramMap.containsKey(ngram)) {
+ 					ngramMap.put(ngram, (ngramMap.get(ngram) + 1));
+ 				} 
+				else {
+ 					ngramMap.put(ngram, 1);
+ 				}
+ 			}
+ 		}
+ 	return ngramMap;
+ }
+}
---
+++ Opdracht2/TestSharon.java
@@ -0,0 +1,321 @@
+import java.util.*;
+import java.io.*;
+import java.util.HashMap;
+
+public class TestSharon {
+ /*
+  * Input: 1. Name of the file to get the n-grams from 2. Size of the n-grams
+  * 3. Amount of highest counts to be printed
+  * 
+  * First get all sentences from the file. Then get and count the n-grams in
+  * those sentences. After that sort the n-grams to get the highest counts.
+  * ovis-trainset.txt 2 10
+  */
+
+	static ArrayList<ArrayList<String>> lines = new ArrayList<ArrayList<String>>();
+	public static void main(String[] args) throws IOException {
+
+		 int n = Integer.parseInt(args[0]);
+		System.out.printf("??? %d \n",n);
+		 String file1 = args[1];
+		 String file2 = args[2];
+		 String file3 = args[3];
+
+		try {
+
+           File file = new File(file1);
+			Scanner s = new Scanner(file);
+
+            // Create an array containing all words in the text (in the same order)
+            while ( s.hasNextLine() ) {
+ 		        ArrayList<String> words = new ArrayList<String>();
+				words.add("START");
+                for ( String word : s.nextLine().split("\\s+") ) {
+
+                    words.add(word);
+
+                }
+				words.add("STOP");
+				lines.add(words);
+		    }
+
+
+			HashMap<String, Integer> ngram = new HashMap<String, Integer>();			
+			HashMap<String, Integer> n_1gram = new HashMap<String, Integer>();
+			ngram = createNgrams(lines, n);
+			n_1gram = createNgrams(lines, n-1);
+
+
+
+			//sort ngram
+			LinkedHashMap<String, Integer> sortedNgrams = new LinkedHashMap<String, Integer>();
+			System.out.println("n gram");
+
+			//SORTEREN GAAT MIS!
+			sortedNgrams = printngrams(ngram, sortedNgrams,n);
+			System.out.println("n-1 gram");
+			LinkedHashMap<String, Integer> sortedN_1grams = new LinkedHashMap<String, Integer>();
+			sortedN_1grams = printngrams(n_1gram, sortedN_1grams,n);			
+
+
+			calculateprob(ngram, n_1gram, n, file2);
+			//print to file
+ 	     	PrintStream out = new PrintStream(new FileOutputStream("OutFileSHARON.txt"));
+			for(Map.Entry<String, Integer> entry : sortedNgrams.entrySet())
+			{
+				String key = entry.getKey();
+				Integer val = entry.getValue();
+				//System.out.printf("%s, %d\n", key, val);
+        		out.println(val+ ": " + key);
+			}
+	      out.close();
+ 	     	PrintStream out2 = new PrintStream(new FileOutputStream("OutFileSHARON_1.txt"));
+			for(Map.Entry<String, Integer> entry : sortedN_1grams.entrySet())
+			{
+				String key = entry.getKey();
+				Integer val = entry.getValue();
+				//System.out.printf("%s, %d\n", key, val);
+        		out2.println(val+ ": " + key);
+			}
+	      out2.close();
+
+
+
+		
+			calculateprob_3(file3);
+
+		} catch ( Exception e ) {
+			// FileNotFoundException
+			e.printStackTrace();
+		}
+	}
+
+	// exercise 2.3
+	public static void calculateprob_3(String filename){
+		
+			try{File file = new File(filename);
+			Scanner s = new Scanner(file);
+            ArrayList<ArrayList<String>> testlines = new ArrayList<ArrayList<String>>();
+            // Create an array containing all words in the text (in the same order)
+            while ( s.hasNextLine() ) {
+ 		        ArrayList<String> words = new ArrayList<String>();
+				words.add("START");
+                for ( String word : s.nextLine().split("\\s+") ) {
+
+                    words.add(word);
+
+                }
+				words.add("STOP");
+				testlines.add(words);
+		    }
+
+
+			System.out.println("Probabilities -------- \n");
+			//loop lines
+			for(int i = 0; i < testlines.size(); i++){
+
+				double count = 1;
+				for(int j = 2; j < testlines.get(i).size(); j++){
+
+					//create ngram with j = n					
+					HashMap<String, Integer> ngram = new HashMap<String, Integer>();			
+					ngram = createNgrams(lines, j);
+					
+					HashMap<String, Integer> n_1gram = new HashMap<String, Integer>();			
+					n_1gram = createNgrams(lines, j-1);
+
+
+
+					//get substring 
+					String substr = "";
+					String substr_n1 = "";
+					double prob1 = 0;
+					double prob2 = 0;
+
+					for(int k = 0; k < j; k++){
+						substr += " " + testlines.get(i).get(k)  ;
+						if(k < j-1)
+							substr_n1 += " " + testlines.get(i).get(k)  ;
+					}	
+
+					//get value of substring from ngram
+					if(ngram.containsKey(substr))
+					{
+						prob1 =  (double)(ngram.get(substr))/ (double)(ngram.size());
+						System.out.println(substr) ;
+						System.out.println(prob1);
+					}
+
+					if(n_1gram.containsKey(substr_n1))
+					{
+						System.out.println(substr_n1);
+						prob2 = (double)(n_1gram.get(substr_n1))/ ((double)n_1gram.size());
+						System.out.println(prob2);
+					}
+					
+					
+					System.out.println((double)prob1/(double)prob2);
+					count = (double)(count * ((double)prob1/(double)prob2) );
+	
+					}
+				System.out.printf("STRING: %s \n", testlines.get(i));			
+
+				System.out.printf("Final count: ");
+				System.out.println(count);	
+			}
+
+			}catch ( Exception e ) {
+			// FileNotFoundException
+			e.printStackTrace();}
+
+		}
+
+	public static void calculateprob(HashMap<String, Integer> ngram, HashMap<String, Integer> n_1gram, int n, String filename){
+			try{File file = new File(filename);
+			Scanner s = new Scanner(file);
+
+            ArrayList<ArrayList<String>> lines = new ArrayList<ArrayList<String>>();
+            // Create an array containing all words in the text (in the same order)
+            while ( s.hasNextLine() ) {
+ 		        ArrayList<String> words = new ArrayList<String>();
+				//words.add("START");
+                for ( String word : s.nextLine().split("\\s+") ) {
+
+                    words.add(word);
+
+                }
+				//words.add("STOP");
+				lines.add(words);
+		    }
+			HashMap<String, Double> prob = new HashMap<String, Double>();			
+			//get wn			
+			for(int i = 0; i < lines.size()-1; i++){
+				//create only for testsentences of length n		
+				int val  = 0;
+				int valn_1 = 0;		
+				if(lines.get(i).size() == n){				
+					String str = "";
+					for(int k = 0; k < lines.get(i).size(); k++){
+							str = str.concat(" " +lines.get(i).get(k));		
+					}
+
+					String substr = "";		
+					//get wn-1				
+					for(int j = 0; j < lines.get(i).size()-1; j++){
+							substr = substr.concat(" " + lines.get(i).get(j));		
+					}
+
+					//get values
+					if(ngram.containsKey(str))
+					{
+						val = ngram.get(str);
+					}
+					if(n_1gram.containsKey(substr))
+					{
+						valn_1 = n_1gram.get(substr);
+					}
+					//calculate prob
+					Double prob_1 = (double) val / (double) valn_1;
+					prob.put(str, prob_1);
+					
+				}
+
+			}
+			System.out.println(prob);
+			}catch ( Exception e ) {
+			// FileNotFoundException
+			e.printStackTrace();
+		}
+	}
+
+	public static LinkedHashMap<String, Integer> printngrams(HashMap<String, Integer> nGramCounts, LinkedHashMap<String, Integer> sortedNgrams, int m){
+
+        ValueComparator bvc =  new ValueComparator(nGramCounts);
+        sortedNgrams.putAll(nGramCounts);
+
+
+
+
+	 /*
+	  * Printing the top m n-grams or all of them if m is bigger than the
+	  * n-gram set
+	  */
+	 Iterator<String> it = sortedNgrams.keySet().iterator();
+	 int i = 0;
+
+
+	 while (it.hasNext()) {
+	 	String nGram = (String) it.next();
+	 	Integer count = sortedNgrams.get(nGram);
+
+	 	if (i < sortedNgrams.size()) {
+	 		i++;
+	 		System.out.println(i + ". " + nGram + ": " + count);
+	 	}
+	 }
+	return sortedNgrams;
+}
+
+
+	public static HashMap<String, Integer> createNgrams(ArrayList<ArrayList<String>> lines, int n){
+		HashMap<String, Integer> ngram = new HashMap<String, Integer>();
+		// loop through all text	
+		for(int i =0; i < lines.size(); i++){
+
+			// loop through sentence if long enough sentence to form ngram!
+			if(lines.get(i).size() > n){
+
+				for(int j = 0; j < lines.get(i).size(); j++){
+					//break if not enough words left for ngram
+
+					if(j+n > lines.get(i).size()){
+						continue;
+					}
+					//create ngram string
+					else{
+						String str = "";
+						for(int k = 0; k < n; k++){
+							str = str + " " + lines.get(i).get(k+j);
+						}
+
+						//if str is known, add value
+						if(ngram.containsKey(str))
+						{
+							Integer val = ngram.get(str);
+							val += 1;
+							ngram.put(str, val);
+						}
+						else{
+							ngram.put(str, 1);	
+						}
+					}
+				}
+
+			}
+
+
+		}
+
+
+		return ngram;
+	}
+
+}
+class ValueComparator implements Comparator {
+
+  Map base;
+  public ValueComparator(Map base) {
+      this.base = base;
+  }
+
+  public int compare(Object a, Object b) {
+
+    if((Double)base.get(a) < (Double)base.get(b)) {
+      return 1;
+    } else if((Double)base.get(a) == (Double)base.get(b)) {
+      return 0;
+    } else {
+      return -1;
+    }
+  }
+}	
